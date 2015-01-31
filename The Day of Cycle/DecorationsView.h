8e0ede58d1750468c8d99d59374c40e8bd823894
aleos <aleos@localhost>
2015-02-01 00:17:07 +0300

    + decorations on the main view

---
+++ The Day of Cycle/DecorationsView.h
@@ -0,0 +1,13 @@
+//
+//  DecorationsView.h
+//  The Day of Cycle
+//
+//  Created by aleos on 31.01.15.
+//  Copyright (c) 2015 Alexander Ostrovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DecorationsView : UIView
+
+@end
