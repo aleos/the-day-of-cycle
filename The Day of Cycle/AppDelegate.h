5f4b5e232190e6f401383adc6c69019245b059b0
aleos <aleos@localhost>
2015-01-31 20:31:50 +0300

    + initial empty project

---
+++ The Day of Cycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The Day of Cycle
+//
+//  Created by aleos on 31.01.15.
+//  Copyright (c) 2015 Alexander Ostrovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ The Day of Cycle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  The Day of Cycle
+//
+//  Created by aleos on 31.01.15.
+//  Copyright (c) 2015 Alexander Ostrovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
