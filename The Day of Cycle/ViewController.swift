571c510bb573f9d5739209b5b088c328d73dbae9
Alexander Ostrovsky <thealeos@gmail.com>
2015-02-22 13:57:19 +0300

    * language change to Swift

---
+++ The Day of Cycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Day of Cycle
+//
+//  Created by aleos on 22.02.15.
+//  Copyright (c) 2015 Alexander Ostrovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
